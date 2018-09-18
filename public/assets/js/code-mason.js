2c0beb5f28ded371a6cc0b3d2302e748c076ff3b
Craig Melville <acekreations@gmail.com>
2018-09-18 11:42:45 -0700

    add snippet model jquery complete. add google login

--- public/assets/js/code-mason.js
+++ public/assets/js/code-mason.js
@@ -21,14 +21,14 @@ function languageSort() {
 //   for (var i = 0; i < /*dataFromServer.length*/; i++) {
 //     languages.push(dataArr[i]);
 //   };
-  
+
   $(".language-sort").html("<h4>Find by Language</h4>" +  "<ul class=languageSortList style='list-style-type:none'>"
   + "</ul>")
 
   for (var i = 0; i < languages.length; i ++) {
     $(".languageSortList").append("<li>" + languages[i] + "</li>")
   }
-  
+
 }
 
 function tagSort() {
@@ -38,10 +38,10 @@ function tagSort() {
     // for (var i = 0; i < /*dataFromServer.length*/; i++) {
     //   tags.push(dataArr[i]);
     // };
-    
+
     $(".tag-sort").html("<h4>Sort by Tag</h4>" +  "<ul class=tagSortList style='list-style-type:none'>"
   + "</ul>")
-  
+
   for (var i = 0; i < tags.length; i ++) {
       $(".tagSortList").append("<li>" + tags[i] + "</li>");
   }
@@ -54,10 +54,10 @@ function recentSnippets() {
     // for (var i = 0; i < /*dataFromServer.length*/; i++) {
     //   recentSnippets.push(dataArr[i]);
     // };
-    
+
     $(".recent-snippets-and-search").html("<h4>Recent Snippets</h4>" +  "<ul class=recentSnippetsList style='list-style-type:none'>"
   + "</ul>");
-  
+
   for (var i = 0; i < recentSnippets.length; i ++) {
       $(".recentSnippetsList").append("<li>" + recentSnippets[i] + "</li>");
   }
@@ -79,8 +79,8 @@ $(document).ready(function() {
 
 /*****************MAIN CONTENT JQUERY ELEMENT GENERATION*************/
 // function renderPopSnippets() {
-  
-//   var popSnippets = [];   
+
+//   var popSnippets = [];
 
 //  $(".snippets-container").append(`<h4>Popular Snippets</h4>`);
 
@@ -100,7 +100,7 @@ function renderUserSnippets() {
 
 //   for (var i = 0; i < /*dataFromServer.length*/; i++) {
 //     userSnippets.push(dataArr[i]);
-//   } 
+//   }
 
   for (var i = 0; i < userSnippets.length; i++) {
     $(".snippets-container").append(`<div class='uk-card uk-card-default'><div class='uk-card-header'><div class='uk-grid-small uk-flex-middle' uk-grid><div class='uk-width-expand'><h3 class='uk-card-title uk-margin-remove-bottom'>`+'Code block title'+`</h3><p class='uk-text-meta uk-margin-remove-top'>`+'Code description. ipsum ipsum.'+`</p></div></div></div><div class='uk-card-body'><p>`+'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'+`</p></div></div>`);
@@ -112,7 +112,60 @@ $(document).ready(function() {
 /**************************Taylor*******************************/
 
 /**************************Craig*******************************/
+$("#snippet-submit").on("click", function(){
+  //create object from form inputs
+  var newSnippet = {
+    title: $("#new-snippet-name").val().trim(),
+    description: $("#new-snippet-descrip").val().trim(),
+    text: $("#new-snippet-code").val().trim(),
+    public: $("input:radio[name='public']:checked").val(),
+    language: $("input:radio[name='lang']:checked").val(),
+    UserId: "123"
+  };
 
+  // get the comma seperated tag value
+  var tags = $("#new-snippet-tags").val().trim();
+  //split by comma into an array
+  tags = tags.split(",");
+
+  //query api to create new code snippet entry
+  $.post("/api/codes", newSnippet, function(res){
+    //new code snippet id returned from query
+    var codeID = res.id;
+
+    //loop tags array
+    for (var i = 0; i < tags.length; i++) {
+      //create object with tag and code snippet id to send in post request
+      var tag = {
+        tagname: tags[i].trim(),
+        codeID: codeID
+      };
+      //send query to enter new tag
+      $.post("/api/tags", tag, function(){
+
+      });
+    }
+  });
+});
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
 
 
 
