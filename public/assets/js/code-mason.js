b6bbf8b6bf5344f95f7a39a19f6a0c3a526dc0d7
Craig Melville <acekreations@gmail.com>
2018-09-18 14:59:45 -0700

    add session storage removal on sign out

--- public/assets/js/code-mason.js
+++ public/assets/js/code-mason.js
@@ -172,10 +172,12 @@ function onSignIn(googleUser) {
     }
   });
 }
-
+//sign user out of app via google sign in
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    sessionStorage.removeItem("userID");
+    window.location.reload();
     console.log('User signed out.');
   });
 }
