3b4c0f6039d5ed1baee5b21a8d4df4ad0adaaf29
Craig Melville <acekreations@gmail.com>
2018-09-18 15:01:38 -0700

    add comments to sign out function

--- public/assets/js/code-mason.js
+++ public/assets/js/code-mason.js
@@ -176,7 +176,9 @@ function onSignIn(googleUser) {
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    //remove session id
     sessionStorage.removeItem("userID");
+    //reload page so the user sees the public view after signing out
     window.location.reload();
     console.log('User signed out.');
   });
